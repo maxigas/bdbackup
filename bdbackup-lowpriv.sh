1d13bbd7a538dc401ad0793e88e96065d01affab
maxigas <maxigas@anargeek.net>
2013-03-12 01:31:31 +0100

    Added quotes and heuristics for picking default arguments.

--- bdbackup-lowpriv.sh
+++ bdbackup-lowpriv.sh
@@ -5,36 +5,38 @@ if [[ $# -ne 5 ]]; then
     exit
 fi
 
-echo '----'     >> ~/bdbackup.log
-echo `date`     >> ~/bdbackup.log
-echo Started, args: $@ >> ~/bdbackup.log
+echo '----'              1>&2
+echo `date`              1>&2
+echo "Started, args: $@" 1>&2
 set -e
 # DEBUG:
-# set -x
-VGLOCAL=$1
-LVLOCAL=$2
-VGREMOTE=$3
-LVREMOTE=$4
-TARGETMACHINE=$5
+set -x
+TARGETMACHINE="$1"
+LVLOCAL="$2"
+VGLOCAL="$3"
+LVREMOTE="$4"
+VGREMOTE="$5"
 DIFF="$HOME/diff.bds.gz"
 
 echo
 echo Dropped privileges. Syncing block devices. This may take a while.
 echo
-bdsync "ssh $TARGETMACHINE bdsync --server" /dev/$VGLOCAL/$LVLOCAL-snap /dev/$VGREMOTE/$LVREMOTE | gzip > $DIFF
-scp $DIFF $TARGETMACHINE:.
-rm $DIFF
+
+bdsync "ssh $TARGETMACHINE bdsync --server" /dev/$VGLOCAL/$LVLOCAL-snap /dev/$VGREMOTE/$LVREMOTE | gzip > "$DIFF"
+scp "$DIFF" "$TARGETMACHINE:."
+rm "$DIFF"
 # TODO:
 # 1. I don't get while I can't write this w/o using a variable.
 #    - Maybe escape pipe?
 # 2. I think bdsync exits with exit value 1 on success.
 #    - Either I am wrong or have to fix bdsync source code...
-CMDREMOTE="gzip -d -c $DIFF | bdsync --patch=/dev/$VGREMOTE/$LVREMOTE"
+CMDREMOTE="gzip -d -c $DIFF | bdsync --patch='/dev/$VGREMOTE/$LVREMOTE'"
 set +e
-ssh $TARGETMACHINE "$CMDREMOTE"
+ssh "$TARGETMACHINE" "$CMDREMOTE"
 set -e
-ssh $TARGETMACHINE rm $DIFF
+ssh "$TARGETMACHINE" rm "$DIFF"
 
-echo `date`     >> ~/bdbackup.log
-echo 'Finished.' >> ~/bdbackup.log
+echo `date`      1>&2
+echo 'Finished.' 1>&2
+
 
--- bdbackup.sh
+++ bdbackup.sh
@@ -17,26 +17,50 @@
 # 1. bdsync:backup user has passwordless ssh access to $TARGETMACHINE
 # 2. This script plus bdbackup-lowpriv.sh is in $PATH and executable.
 
-if [[ $# -ne 5 ]]; then
-    echo "Usage: ${0##*/} VGLOCAL LVLOCAL VGREMOTE LVREMOTE TARGETMACHINE"
-    exit
-fi
+USAGE="Usage: ${0##*/} TARGETMACHINE LVLOCAL VGLOCAL LVREMOTE VGREMOTE"
+
 set -e
 # DEBUG:
-# set -x
-ARGS=$@
-VGLOCAL=$1
-LVLOCAL=$2
-VGREMOTE=$3
-LVREMOTE=$4
-TARGETMACHINE=$5
+#set -x
+ARGS="$@"
+
+if [ -z "$1" -o -z "$2" ]; then
+    echo $USAGE; exit 1
+else
+    TARGETMACHINE="$1"
+    LVLOCAL="$2"
+fi
+
+if [ -z "$3" ]; then
+    VGLOCAL="vg_$(hostname)0"
+else
+    VGLOCAL="$3"
+fi
+
+if [ -z "$4" ]; then
+    LVREMOTE="$LVLOCAL-backup"
+else
+    LVREMOTE="$4"
+fi
+
+if [ -z "$5" ]; then
+    VGREMOTE=$(echo $TARGETMACHINE | cut -d '.' -f1)
+else
+    VGREMOTE="$5"
+fi
+
+if [ -n "$6" ]; then
+    echo $USAGE; exit 2
+fi
 
 echo
 read -p "Sync local /dev/$VGLOCAL/$LVLOCAL with /dev/$VGREMOTE/$LVREMOTE on $TARGETMACHINE (via a snapshot)? (y/n) "
-if [ ! $REPLY == "y" ]; then exit 1; fi
+if [ ! $REPLY == "y" ]; then
+    exit 1
+fi
 
-lvcreate --snapshot -L 1G -p r --name $LVLOCAL-snap /dev/$VGLOCAL/$LVLOCAL
-su - bdsync -c "bdbackup-lowpriv.sh $ARGS"
-lvremove -f /dev/$VGLOCAL/$LVLOCAL-snap
+lvcreate --snapshot -L 1G -p r --name "$LVLOCAL-snap" "/dev/$VGLOCAL/$LVLOCAL"
+su - bdsync -c "bdbackup-lowpriv.sh $ARGS" 2> /var/log/bdsync.log
+lvremove -f "/dev/$VGLOCAL/$LVLOCAL-snap"
 echo READY
 
